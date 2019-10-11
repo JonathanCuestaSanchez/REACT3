f9c1ba97cf1edc41282b649dd7b892628dbd382d
JonathanCuestaSanchez <jonathancuesta97@gmail.com>
2019-10-10 19:51:18 -0500

    offline

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,8 @@
 var doCache = true; // Set this to true for production
 
 var CACHE_NAME = "my-pwa-cache-v1";
-const FILES_TO_CACHE = [
-  '/offline.html',
-];
 
 self.addEventListener("activate", event => {
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-);
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(keyList =>
@@ -32,12 +19,6 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("install", function(event) {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching offline page');
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
   if (doCache) {
     event.waitUntil(
       caches.open(CACHE_NAME).then(function(cache) {
@@ -56,19 +37,6 @@ self.addEventListener("install", function(event) {
 });
 
 self.addEventListener("fetch", function(event) {
-  if (evt.request.mode !== 'navigate') {
-    // Not a page navigation, bail.
-    return;
-  }
-  evt.respondWith(
-      fetch(evt.request)
-          .catch(() => {
-            return caches.open(CACHE_NAME)
-                .then((cache) => {
-                  return cache.match('offline.html');
-                });
-          })
-  );
   if (doCache) {
     event.respondWith(
       caches.match(event.request).then(function(response) {
